4fdc5b3aeb913313093286588647c6bd0fa49159
Huỳnh Phương Duy <kudophuongduy@gmail.com>
2018-05-26 23:15:53 +0700

    update

---
+++ Ztai/Bird 2D/Assets/Scripts/MenuMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMovement : MonoBehaviour {
+	public float bounceForce;
+	private Rigidbody2D myBody;
+	// Use this for initialization
+	void Start () {
+		myBody = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.y < 3) {
+			myBody.gravityScale = -3;
+			myBody.velocity = new Vector2 (myBody.velocity.x,bounceForce);
+		} else {
+			myBody.gravityScale = 3;
+			myBody.velocity = new Vector2(myBody.velocity.x,- bounceForce);
+		}
+		}
+	}
+
