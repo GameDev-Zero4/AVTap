1c244bb320b9384dc26197130f0c128f37f30af4
Huỳnh Phương Duy <kudophuongduy@gmail.com>
2018-05-26 13:02:36 +0700

    update

---
+++ Ztai/Bird 2D/Assets/Scripts/Rotator60.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator60 : MonoBehaviour {
+	public float speedRotate;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while(transform.rotation.z<150)
+			transform.Rotate(new Vector3(0,0,transform.rotation.z+speedRotate)*Time.deltaTime);
+		while(transform.rotation.z>210)
+			transform.Rotate(new Vector3(0,0,transform.rotation.z-speedRotate)*Time.deltaTime);
+	}
+}
