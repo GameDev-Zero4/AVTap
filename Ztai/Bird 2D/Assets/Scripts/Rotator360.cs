48954f3408941540400c05808b346cb42547710b
HuynhTanDuy <36908337+HuynhTanDuy@users.noreply.github.com>
2018-05-26 14:19:33 +0700

    Up script hammer 60

--- Ztai/Bird 2D/Assets/Scripts/Rotator360.cs
+++ Ztai/Bird 2D/Assets/Scripts/Rotator360.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Rotator360 : MonoBehaviour {
 
 	public float speedRotate;
-	// Use this for initialization
+  	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (0, 0, speedRotate) * Time.deltaTime * 2);
+        transform.Rotate (new Vector3 (0, 0, speedRotate) * Time.deltaTime * 2);
+       
 	}
 }
--- Ztai/Bird 2D/Assets/Scripts/Rotator60.cs
+++ Ztai/Bird 2D/Assets/Scripts/Rotator60.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator60 : MonoBehaviour {
-	public float speedRotate;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		while(transform.rotation.z<150)
-			transform.Rotate(new Vector3(0,0,transform.rotation.z+speedRotate)*Time.deltaTime);
-		while(transform.rotation.z>210)
-			transform.Rotate(new Vector3(0,0,transform.rotation.z-speedRotate)*Time.deltaTime);
-	}
+    public float speedRotate;
+   
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (new Vector3 (0, 0, speedRotate) * Time.deltaTime * 2);
+       
+    }
 }
