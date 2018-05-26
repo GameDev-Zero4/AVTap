71862000cd9ea00c1b50dc0c83b5123f345a1c05
Huỳnh Phương Duy <kudophuongduy@gmail.com>
2018-05-26 14:28:49 +0700

    update

--- Ztai/Bird 2D/Assets/Scripts/Rotator60.cs
+++ Ztai/Bird 2D/Assets/Scripts/Rotator60.cs
@@ -14,7 +14,7 @@ public class Rotator60 : MonoBehaviour {
 	void Update () {
         //transform.Rotate (new Vector3 (0, 0, speedRotate) * Time.deltaTime * 2);
         this.angle += Time.deltaTime * this.speedRotate;
-        float a = Mathf.Sin(angle) * 85;
+        float a = Mathf.Sin(angle) * 60;
         this.transform.localEulerAngles = new Vector3(0, 0, a);
 	}
 }
