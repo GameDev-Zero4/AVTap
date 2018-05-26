e998195afb316a7a2e7d63073821199c63ae24c6
HuynhTanDuy <36908337+HuynhTanDuy@users.noreply.github.com>
2018-05-26 14:23:27 +0700

    Up script 60

--- Ztai/Bird 2D/Assets/Scripts/Rotator60.cs
+++ Ztai/Bird 2D/Assets/Scripts/Rotator60.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator60 : MonoBehaviour {
-    public float speedRotate;
-   
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Rotate (new Vector3 (0, 0, speedRotate) * Time.deltaTime * 2);
-       
-    }
+		public float speedRotate;
+    private float angle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Rotate (new Vector3 (0, 0, speedRotate) * Time.deltaTime * 2);
+        this.angle += Time.deltaTime * this.speedRotate;
+        float a = Mathf.Sin(angle) * 85;
+        this.transform.localEulerAngles = new Vector3(0, 0, a);
+	}
 }
