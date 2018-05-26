4a50c48b795a7ff033c5f4ce5158bfa18f0308bc
lhsang <lehoangsangcpp@gmail.com>
2018-05-26 10:37:32 +0700

    begin

---
+++ Ztai/Bird 2D/Assets/Scripts/AutoMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMovement : MonoBehaviour {
+
+	private Rigidbody2D myBody;
+	public float speed;
+	// Use this for initialization
+	void Awake()
+	{
+
+		myBody = GetComponent<Rigidbody2D>();
+
+
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+
+
+	}
+}
---
+++ Ztai/Bird 2D/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float bounceForce;
+	public GameObject bubble;
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    void Awake()
+    {
+        
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+      
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move();
+	}
+
+    void Move()
+    {
+        if (transform.position.y > 0)
+        {
+			bubble.SetActive (false);
+            myBody.gravityScale = 3;
+			if (Input.GetKey(KeyCode.Space))
+            {
+                myBody.velocity = new Vector2(myBody.velocity.x, bounceForce);
+            }
+        }
+        else
+        {
+			bubble.SetActive (true);
+            myBody.gravityScale = -3;
+			if (Input.GetKey(KeyCode.Space))
+            {
+                myBody.velocity = new Vector2(myBody.velocity.x, -bounceForce);
+            }
+        }
+    }
+}
